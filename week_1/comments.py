adf5ba04c56a5a579286f1d8e5ef0145552b36a0
smunene <wmstanslaus@gmail.com>
2024-02-15 10:00:05 +0300

    third commit

---
+++ week_1/comments.py
@@ -0,0 +1,21 @@
+# this is a single line comment
+# program written by Stanslaus
+# Date : 15/02/2024
+first_name = "Stanslaus"
+second_name = "Wachira"
+print("My first name is :" , first_name)
+print("My second name is :" , second_name)
+
+# program to show my age 
+age = 18
+print("I am "+ str(age) + "years old")
+
+"""
+This is a multiline comment
+meaning
+it
+has
+many
+lines
+"""
+
---
+++ week_1/lesson_two.py
@@ -0,0 +1,7 @@
+print("My name is Stanslaus")
+print("I am 18 years old")
+print("I come from Nairobi County")
+print("I love acting, dancing, watching")
+print("---------------")
+print("****************")
+print("###############")
