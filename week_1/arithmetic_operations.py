e5793cb25e8fbab9addb70fc5dd8f44d0309df13
smunene <wmstanslaus@gmail.com>
2024-02-15 22:32:14 +0300

    third commit

--- week_1/arithmetic_operations.py
+++ week_1/arithmetic_operations.py
@@ -14,3 +14,20 @@ print("The sum of the numbers ",sum_numbers)
 print("The diff of the numbers ",diff_numbers)
 print("The product of the numbers ",product_numbers)
 print("The quotient of the numbers ",quotient_numbers)
+
+first_number = 10
+second_number = 12
+third_number = 15
+fourth_number = 17
+fifth_number = 25
+sixth_number = 28
+seventh_number = 40
+eighth_number = 50
+nineth_number = 54
+tenth_number = 60
+
+sum_numbers = first_number + second_number + third_number + fourth_number + fifth_number + sixth_number + seventh_number + eighth_number + nineth_number + tenth_number
+average_numbers = (first_number + second_number + third_number + fourth_number + fifth_number + sixth_number + seventh_number + eighth_number + nineth_number + tenth_number) / 10
+
+print("The sum of the numbers" ,sum_numbers)
+print("The average of the numbers" ,average_numbers)
