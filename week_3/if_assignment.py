a6c312265751f6242ae5bbe282f121b26810fe11
smunene <wmstanslaus@gmail.com>
2024-02-26 23:26:04 +0300

    fourth commit

---
+++ week_3/if_assignment.py
@@ -0,0 +1,16 @@
+# This is a program to show if conditions
+# Date : 26/02/2024
+# Name : Stanslaus
+
+sal = 170000
+if sal > 100000 :
+    salary = sal * 1.3
+    print(salary)
+
+if sal >= 100000 and sal <= 150000 :
+    salary = sal * 1.15
+    print(salary)
+
+if sal > 150000 :
+    salary = sal * 1.05
+    print(salary)
